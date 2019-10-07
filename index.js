a56d7b54ee18dc93da2e48d055630e5ef5134099
Andre Staltz <andre@staltz.com>
2019-10-07 17:24:12 +0300

    fix one misnamed 'sodium-native-no-prebuild'

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var sodium = require('bindings')({
   bindings: 'sodium.node',
-  name: 'sodium-native-no-prebuild'
-})
+  name: 'sodium-native-nodejs-mobile',
+});
 
-module.exports = sodium
+module.exports = sodium;
